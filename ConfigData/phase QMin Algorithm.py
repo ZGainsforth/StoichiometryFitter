e83d9dfcbf7a9b19fbe70675c005f05c44f3a419
Michael Ofengenden <92415438+michaelofengend@users.noreply.github.com>
2022-08-27 15:35:47 -0700

    Update with returned results

--- ConfigData/phase QMin Algorithm.py
+++ ConfigData/phase QMin Algorithm.py
@@ -22,6 +22,7 @@ if __name__ != '__main__':
     import PhysicsBasics as pb
 
 def AnalyzePhase(AtPct=None, WtPct=None, OxWtPct=None, OByStoich=None):
+    global OutStr
     OutStr = '--- QMin Mineral Analysis ---\n\n'
     #KnownElements = ['O', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni']
     Known2lements = ['Si', 'Ti', 'Al', 'Fe', 'Mn', 'Mg', 'Ca', 'Ba', 'Sr', 'Pb', 'Na', 'K', 'Rb']
@@ -29,6 +30,7 @@ def AnalyzePhase(AtPct=None, WtPct=None, OxWtPct=None, OByStoich=None):
     frstRow = ['Point', 'SiO2', 'TiO2', 'Al2O3', 'FeOT', 'MnO', 'MgO', 'CaO', 'BaO', 'SrO', 'PbO', 'Na2O', 'K2O', 'Rb2O', 'Total']
     print(frstRow)
     scndRow = ['1']
+    OutStr += "INPUT DATA:\n"
     for Element in Known2lements:
         x = round(eval('OxWtPct[pb.%s-1]'%(Element)), 4)
         E[Element] = x
@@ -56,15 +58,30 @@ def QminAlgo():
         driver.get('https://apps.cprm.gov.br/qmin/')
     except:
         print("User must install Chrome to use this method of QMin Analysis.")
-    time.sleep(2)
+    time.sleep(1)
     element = driver.find_element(By.ID, "upload-data")
     files = os.getcwd()+"/qminFormat.csv"
     JS_DROP_FILES = "var k=arguments,d=k[0],g=k[1],c=k[2],m=d.ownerDocument||document;for(var e=0;;){var f=d.getBoundingClientRect(),b=f.left+(g||(f.width/2)),a=f.top+(c||(f.height/2)),h=m.elementFromPoint(b,a);if(h&&d.contains(h)){break}if(++e>1){var j=new Error('Element not interactable');j.code=15;throw j}d.scrollIntoView({behavior:'instant',block:'center',inline:'center'})}var l=m.createElement('INPUT');l.setAttribute('type','file');l.setAttribute('multiple','');l.setAttribute('style','position:fixed;z-index:2147483647;left:0;top:0;');l.onchange=function(q){l.parentElement.removeChild(l);q.stopPropagation();var r={constructor:DataTransfer,effectAllowed:'all',dropEffect:'none',types:['Files'],files:l.files,setData:function u(){},getData:function o(){},clearData:function s(){},setDragImage:function i(){}};if(window.DataTransferItemList){r.items=Object.setPrototypeOf(Array.prototype.map.call(l.files,function(x){return{constructor:DataTransferItem,kind:'file',type:x.type,getAsFile:function v(){return x},getAsString:function y(A){var z=new FileReader();z.onload=function(B){A(B.target.result)};z.readAsText(x)},webkitGetAsEntry:function w(){return{constructor:FileSystemFileEntry,name:x.name,fullPath:'/'+x.name,isFile:true,isDirectory:false,file:function z(A){A(x)}}}}}),{constructor:DataTransferItemList,add:function t(){},clear:function p(){},remove:function n(){}})}['dragenter','dragover','drop'].forEach(function(v){var w=m.createEvent('DragEvent');w.initMouseEvent(v,true,true,m.defaultView,0,0,0,b,a,false,false,false,false,0,null);Object.setPrototypeOf(w,null);w.dataTransfer=r;Object.setPrototypeOf(w,DragEvent.prototype);h.dispatchEvent(w)})};m.documentElement.appendChild(l);l.getBoundingClientRect();return l"
     input = driver.execute_script(JS_DROP_FILES, element)
     input._execute('sendKeysToElement', {'value': [files], 'text': files})
+    time.sleep(4)
+    resultGroup = driver.find_element(By.CLASS_NAME, "cell-table")
+    fullTable = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[2]')
+    global OutStr
+    info = fullTable.text.splitlines()[-1]
+    ress  = resultGroup.text.splitlines()[-1]
+    OutStr += "\n\nRESULTS:\n"
+    OutStr += f"Predicted Group:          {ress.split()[0]}\n"
+    OutStr += f"Quality Control:          {ress.split()[1]} {ress.split()[2]}\n"
+    OutStr += f"Predicted Mineral:        {info.split()[0]}\n"
+    OutStr += f"Quality Control:          {info.split()[1]} {info.split()[2]}\n"
+    OutStr += f"Second Predicted Mineral: {info.split()[3]}"
+
+
 
 if __name__ == '__main__':
     import imp
     pb = imp.load_source('PhysicsBasics', '../PhysicsBasics.py')
     AnalyzePhase()
+    
     
