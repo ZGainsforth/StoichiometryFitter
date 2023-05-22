dfe9a7cbb96c3183d6c267fd4d6f00a2098a03e2
Roger Yu <roger_yu@berkeley.edu>
2023-05-22 10:34:39 +0800

    FIX GEMS

--- ConfigData/phase GEMS Comparison.py
+++ ConfigData/phase GEMS Comparison.py
@@ -3,6 +3,12 @@ __author__ = 'Zack Gainsforth'
 __copyright__ = 'Copyright 2015, Zack Gainsforth'
 __email__ = 'zsg@gainsforth.com'
 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+import json
+import plotly
+
 import matplotlib
 import matplotlib.pyplot as plt
 from numpy import *
@@ -133,11 +139,11 @@ def AnalyzePhase(AtPct=None, WtPct=None, OxWtPct=None, OByStoich=None):
     ProtosolarToFe /= ProtosolarToFe[pb.Fe-1]
     ProtosolarChondricity = ProtosolarToFe
 
-    GEMSAtPct, GEMSAtPctSD = GEMSPlot(AtPct, ProtosolarToSi)
+    GEMSAtPct, GEMSAtPctSD= GEMSPlot(AtPct, ProtosolarToSi)
 
     #PrintTernary(AtPct, GEMSAtPct, GEMSAtPctSD)
 
-    Chondricity, ElementSigma, GEMSChiSqRed, DOF = GEMSChiPlot(AtPct, Protosolar)
+    Chondricity, ElementSigma, GEMSChiSqRed, DOF= GEMSChiPlot(AtPct, Protosolar)
 
     ShowLastPos(plt)
 
@@ -177,6 +183,96 @@ def AnalyzePhase(AtPct=None, WtPct=None, OxWtPct=None, OByStoich=None):
               'http://doi.org/10.1016/j.gca.2011.06.040'
 
     return OutStr
+
+def AnalyzePhaseWeb(AtPct=None, WtPct=None, OxWtPct=None, OByStoich=None):
+
+    #Normalize our AtPct vector.
+    AtPct = AtPct/sum(AtPct)*100
+
+    # A dictionary of the AtPct values would be useful so we can look up by element name.
+    E = dict(list(zip(pb.ElementalSymbols, AtPct)))
+
+
+    ### We output an output string which contains Mg, Si and Fe ratioed values.
+    OutStr = '--- Simple At% ratios ---\n\n'
+
+    OutStr += "Abundances ratioed to:\n"
+    OutStr += "Element to   Mg       Si       Fe\n"
+    OutStr += '-'*41 + '\n'
+    for Zminus, E in enumerate(AtPct):
+        if E != 0:
+            EtoMg = E / AtPct[pb.Mg-1]
+            EtoSi = E / AtPct[pb.Si-1]
+            EtoFe = E / AtPct[pb.Fe-1]
+            OutStr += '%-13s%-9.3f%-9.3f%-9.3f\n' % (tuple([pb.ElementalSymbols[Zminus+1]]) + tuple([EtoMg, EtoSi, EtoFe]))
+
+
+
+    ### We output an output string which contains ratios to chondritic (protosolar).
+    OutStr += '--- Chondritic Analysis ---\n\n'
+
+    # Load the prosolar abundances.  This is recorded from the Lodders ref with logarithmic values.
+    ProtosolarAbundancesFileName = 'ProtosolarAbundances.csv'
+    if not os._exists(ProtosolarAbundancesFileName):
+        ProtosolarAbundancesFileName = os.path.join('ConfigData', ProtosolarAbundancesFileName)
+    Protosolar = genfromtxt(ProtosolarAbundancesFileName, delimiter=',', skip_header=1, dtype=None)
+    ProtosolarDict = dict(Protosolar)   # This dictionary could be handy...
+    Protosolar = array(list(zip(*Protosolar))[1]) # But we really need just a numpy array with the numbers.
+
+    # Convert to vectors which are normalized to Mg, Si, and Fe.
+    ProtosolarToMg = power(10, Protosolar)  # Get out of log space into linear space.  Now the numbers relate to AtPct.
+    ProtosolarToMg /= ProtosolarToMg[pb.Mg-1]
+    ProtosolarToSi = power(10, Protosolar)
+    ProtosolarToSi /= ProtosolarToSi[pb.Si-1]
+    ProtosolarToFe = power(10, Protosolar)
+    ProtosolarToFe /= ProtosolarToFe[pb.Fe-1]
+    ProtosolarChondricity = ProtosolarToFe
+
+    GEMSAtPct, GEMSAtPctSD, plot1 = GEMSPlotWeb(AtPct, ProtosolarToSi)
+
+    #PrintTernary(AtPct, GEMSAtPct, GEMSAtPctSD)
+
+    Chondricity, ElementSigma, GEMSChiSqRed, DOF, plot2 = GEMSChiPlotWeb(AtPct, Protosolar)
+
+    ShowLastPos(plt)
+
+    # Print out the abundances normalized to protosolar.
+    Ratios = list() # Keep track of the ratios, so at the end we can compute standard deviations.
+    OutStr += "Abundances ratioed to protosolar and normalized to:\n"
+    OutStr += "Element to   Mg       Si       Fe      Chondricity GEMSSigmas\n"
+    OutStr += '-'*60 + '\n'
+    for Zminus, E in enumerate(AtPct):
+        if E != 0:
+            EtoMg = E / AtPct[pb.Mg-1] # Norm to Mg
+            EtoSi = E / AtPct[pb.Si-1] # Si
+            EtoFe = E / AtPct[pb.Fe-1] # Fe
+            Ratios.append([EtoMg/ProtosolarToMg[Zminus], # Norm Mg and protosolar
+                           EtoSi/ProtosolarToSi[Zminus], # Si and protosolar
+                           EtoFe/ProtosolarToFe[Zminus], # Fe and protosolar
+                           Chondricity[Zminus],         # Chondritic-like ratio.
+                           ElementSigma[Zminus]])          # How many sigmas away from GEMS mean per element.
+            OutStr += '%-13s%-9.3f%-9.3f%-9.3f%-11.3f%-9.3f\n' % (tuple([pb.ElementalSymbols[Zminus+1]]) + tuple(Ratios[-1]))
+    Ratios = array(Ratios)
+    Means = nanmean(Ratios, axis=0)
+    Stdevs = nanstd(Ratios, axis=0)
+    OutStr += '-'*60 + '\n'
+    OutStr += '%-13s%-9.3f%-9.3f%-9.3f%-11.3f%-9.3f\n' % (tuple(['Mean']) + tuple(Means))
+    OutStr += '%-13s%-9.3f%-9.3f%-9.3f%-11.3f%-9.3f\n' % (tuple(['Standard dev']) + tuple(Stdevs))
+    OutStr += 'GEMS reduced ChiSquared: %g, with DOF: %g\n' % (GEMSChiSqRed, DOF)
+    OutStr += '-'*60 + '\n'
+
+    OutStr += '\nRefs:\n    Lodders, K. (2003). Solar System Abundances and Condensation Temperatures of the Elements. The Astrophysical ' \
+              'Journal, 591(2), 1220-1247. http://doi.org/10.1086/375492\n' \
+              '    Ishii, H. A., et al. (2008). Comparison of Comet 81P/Wild 2 Dust with Interplanetary Dust from Comets. Science, ' \
+              '319(5), 447. http://doi.org/10.1126/science.1150683\n' \
+              '    Messenger, S., Nakamura-Messenger, K., Keller, L. P., & Clemett, S. J. (2015). ' \
+              'Pristine stratospheric collection of interplanetary dust on an oil-free polyurethane foam substrate. Meteoritics and Planetary Science, ' \
+              '50(8), 1468-1485. http://doi.org/10.1111/maps.12473\n' \
+              '    Keller, L. P., & Messenger, S. (2011). On the origins of GEMS grains. Geochimica Et Cosmochimica Acta, 75(18), 5336-5365. ' \
+              'http://doi.org/10.1016/j.gca.2011.06.040'
+
+    return OutStr, plot1, plot2
+
 
 def GEMSChiPlot(AtPct, Protosolar):
     AtPct = copy(AtPct[:len(Protosolar)]) # We have to truncate the AtPct vector so it has the same elements as the Protosolar list.
@@ -239,7 +335,6 @@ def GEMSChiPlot(AtPct, Protosolar):
     # print('xmean: ', xmean)
     # print('Chi Squared result from scipy:', ScipyChiSquare)
 
-
     plt.figure(3)
     plt.clf()
     IncludedZ = where(~isnan(Chondricity))[0]+1
@@ -252,7 +347,7 @@ def GEMSChiPlot(AtPct, Protosolar):
     plt.legend()
     plt.ylabel('Chondricity:\nlog$_{10}$(At%/Chondritic)\nVariation from GEMS ($\sigma$)', fontsize=FontSizeBasis)
     plt.tight_layout()
-    # print(plt.get_backend())
+    print(plt.get_backend())
 
     # print('Chondricity values: ', Chondricity[IncludedZ-1])
     # print('Chondricity mean: %g, Chondricity std: %g' % (nanmean(Chondricity), nanstd(Chondricity)))
@@ -260,6 +355,161 @@ def GEMSChiPlot(AtPct, Protosolar):
     # print('GEMSChiSqRed: %g' % GEMSChiSqRed)
 
     return Chondricity, ElementSigma, GEMSChiSqRed, DOF
+
+def GEMSChiPlotWeb(AtPct, Protosolar):
+    AtPct = copy(AtPct[:len(Protosolar)]) # We have to truncate the AtPct vector so it has the same elements as the Protosolar list.
+    AtPct[AtPct==0] = nan # We don't want zero abundance elements included in the mean.
+
+    # We also need a trimmed version that will remove elements that are too low Z for comparison with GEMS, or for which we don't have GEMS averages.  Otherwise, it messes up the DOF in the ChiSquared calculation.
+    AtPctTrimmed = copy(AtPct[:len(Protosolar)])
+    AtPctTrimmed[AtPctTrimmed == 0] = nan # We don't want zero abundance elements included in the mean.
+    AtPctTrimmed[:pb.Na-1] = nan # Ignore anything below Na as it is too volatilie for GEMS comparison.
+
+    ProtosolarChondricity = power(10, Protosolar)
+    ProtosolarChondricity[isnan(AtPct)] = nan
+
+    GEMSAtPct, GEMSAtPctSD, RefStr = GetGEMSStandardVals(len(AtPctTrimmed))
+    GEMSAtPct[GEMSAtPct == 0] = nan # We don't want zero abundance elements included in the mean.
+    GEMSAtPctSD /= GEMSAtPct # Make these relative error bars or things get messed up when we renormalize.
+
+    # Sometimes, the user is quanting a spectrum that has extra or missing elements compared to the GEMS average, and the protosolar values always have every element.
+    # To compare these, we have to take the intersection elements only, and norm with those.
+    GEMSAtPct[isnan(AtPctTrimmed)] = nan # Remove elements that are part of the standard GEMS elements but the user didn't quant.
+    AtPctTrimmed[isnan(GEMSAtPct)] = nan # Remove elements that the user did quant but are not in the standard GEMS elements.
+    GEMSAtPctSD[isnan(GEMSAtPct)] = nan # And the error bars for the GEMS standards have to be trimmed to.
+
+    # Now we need to renormalize everything so they can be compared.
+    Normit = 10
+    AtPct /= nansum(AtPct) * Normit
+    AtPctTrimmed /= nansum(AtPctTrimmed) * Normit
+    GEMSAtPct /= nansum(GEMSAtPct) * Normit
+    ProtosolarChondricity /= nansum(ProtosolarChondricity) * Normit
+
+    # Now that things are renormalized, we can turn the GEMS standard deviations back into actual standard deviations instead of relative deviations.
+    GEMSAtPctSD *= GEMSAtPct
+    # print('GEMSAtPctSD', GEMSAtPctSD)
+    # print('GEMSAtPct', GEMSAtPct)
+
+    # Compute the chondricity.
+    Chondricity = log10(AtPct/ProtosolarChondricity)
+
+    # Compute how many sigmas away from the mean each element is.
+    ElementSigma = (AtPctTrimmed-GEMSAtPct)/GEMSAtPctSD
+    # Compute the chi-square values for each element.
+    # ElementChiSq = (AtPctTrimmed-GEMSAtPct)**2/GEMSAtPct
+    ElementChiSq = (AtPctTrimmed-GEMSAtPct)**2/GEMSAtPctSD**2
+
+    # We assume normal distributions for each element which is wrong -- but this still produced a scalar metric which is better than none.
+    # Todo in future work: JointGEMSChi using joint distributions based off GEMS compositions from Keller and Messenger 2011.
+    NumElements = nansum(~isnan(GEMSAtPct))
+    DOF = NumElements-1 # We have a small number of elements.  Better to use the unbiased estimator.
+
+    # And the reduced chisquared value.
+    GEMSChiSqRed = nansum(ElementChiSq)/DOF
+
+    # # Also compute this using scipy stats for kicks.
+    # x = AtPctTrimmed
+    # xmean = GEMSAtPct
+    # x = x[~isnan(x)]
+    # xmean = xmean[~isnan(xmean)]
+    # ScipyChiSquare = chisquare(x, xmean)
+    # print('x: ', x)
+    # print('xmean: ', xmean)
+    # print('Chi Squared result from scipy:', ScipyChiSquare)
+
+
+    # Plotly
+    IncludedZ = np.where(~np.isnan(Chondricity))[0]+1
+    TickLabels = [El for Z, El in enumerate(pb.ElementalSymbols) if Z in IncludedZ]
+    TickInds = list(range(len(TickLabels)))
+
+    fig = make_subplots()
+
+    # Scatter plot for Chondricity
+    trace1 = go.Scatter(
+        x=TickInds,
+        y=Chondricity[IncludedZ-1],
+        mode='markers',
+        marker=dict(
+            size=15,
+            color='red',
+            opacity=0.5
+        ),
+        name='Chondricity'
+    )
+
+    # Scatter plot for Elemental sigmas from mean
+    trace2 = go.Scatter(
+        x=TickInds,
+        y=ElementSigma[IncludedZ-1],
+        mode='markers',
+        marker=dict(
+            size=15,
+            color='blue',
+            opacity=0.5
+        ),
+        name='Elemental sigmas from mean'
+    )
+
+    # Line at Chondritic/GEMSitic
+    trace3 = go.Scatter(
+        x=[0, 92],
+        y=[0, 0],
+        mode='lines',
+        line=dict(
+            color='green',
+            width=3
+        ),
+        name='Chondritic/GEMSitic'
+    )
+
+    fig.add_trace(trace1)
+    fig.add_trace(trace2)
+    fig.add_trace(trace3)
+
+    # Set x-axis labels
+    fig.update_xaxes(
+        tickvals=TickInds,
+        ticktext=TickLabels,
+        tickangle=90
+    )
+
+    fig.update_layout(
+        yaxis_title='Chondricity:\nlog$_{10}$(At%/Chondritic)\nVariation from GEMS ($\sigma$)',
+        autosize=True,
+        legend=dict(
+            orientation='h',
+            yanchor='bottom',
+            y=1.02,
+            xanchor='right',
+            x=1
+        )
+    )
+
+    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # fig.show()
+
+    # plt.figure(3)
+    # plt.clf()
+    # IncludedZ = where(~isnan(Chondricity))[0]+1
+    # TickLabels = [El for Z, El in enumerate(pb.ElementalSymbols) if Z in IncludedZ]
+    # TickInds = list(range(len(TickLabels)))
+    # plt.scatter(TickInds, Chondricity[IncludedZ-1], marker='o', color='red', s=150, alpha=0.5, label='Chondricity')
+    # plt.scatter(TickInds, ElementSigma[IncludedZ-1], marker='o', color='blue', s=150, alpha=0.5, label='Elemental sigmas from mean')
+    # plt.xticks(TickInds, TickLabels, rotation='vertical')
+    # plt.axhline(0, 0, 92, color='green', linewidth=3, label='Chondritic/GEMSitic')
+    # plt.legend()
+    # plt.ylabel('Chondricity:\nlog$_{10}$(At%/Chondritic)\nVariation from GEMS ($\sigma$)', fontsize=FontSizeBasis)
+    # plt.tight_layout()
+    # print(plt.get_backend())
+
+    # print('Chondricity values: ', Chondricity[IncludedZ-1])
+    # print('Chondricity mean: %g, Chondricity std: %g' % (nanmean(Chondricity), nanstd(Chondricity)))
+    # print('ElementSigma values: ', ElementSigma[IncludedZ-1])
+    # print('GEMSChiSqRed: %g' % GEMSChiSqRed)
+
+    return Chondricity, ElementSigma, GEMSChiSqRed, DOF, fig
 
 
 def GetGEMSStandardVals(AtPctVectorLen, WhichRefs='KMMK'):
@@ -346,6 +596,7 @@ def GEMSPlot(AtPct, ProtosolarToSi):
         #     ChondriticInds.append(TickLabels.index(pb.ElementalSymbols[Zminus1+1]))
         #     ChondriticVals.append(ProtosolarToSi[Zminus1])
     # We will be plotting so clear the plot that may already be plotted.
+
     plt.figure(1)
     plt.clf()
     # GEMS plot
@@ -364,8 +615,163 @@ def GEMSPlot(AtPct, ProtosolarToSi):
     plt.ylabel('Element/Si/chondritic, At%', fontsize=FontSizeBasis)
     plt.gca().set_ylim([3e-2, 30])
     plt.tight_layout()
+
+
+
     return GEMSAtPct, GEMSAtPctSD
 
+def GEMSPlotWeb(AtPct, ProtosolarToSi):
+    ### Draw a plot comparing this spectrum normalized to CI and plotted against GEMS compositions.
+    # First we have mean and standard deviation values for GEMS compositions.
+    GEMSAtPct, GEMSAtPctSD, RefStr = GetGEMSStandardVals(pb.U-1)
+    # Make these Si normalized.
+    SiTemp = GEMSAtPct[pb.Si - 1]
+    # GEMSRel is derived from GEMSAtPct, but is normalized against Si and normalized against chondritic.
+    GEMSRel = copy(GEMSAtPct) / SiTemp
+    GEMSRelSD = copy(GEMSAtPctSD) / SiTemp
+    # And normalize to chondritic
+    GEMSRel /= ProtosolarToSi[0:pb.U - 1]
+    GEMSRelSD /= ProtosolarToSi[0:pb.U - 1]
+    # Make a version of the sample quant which is ratioed to si
+    AtPctToSi = AtPct / AtPct[pb.Si - 1]
+    # And chondritic
+    AtPctToSi[:len(ProtosolarToSi)] /= ProtosolarToSi
+    # Get the union of elements which are in our spectrum and in the GEMS mean values.
+    # All indices for elements which have non zero values from either array.
+    IncludedZ = hstack((nonzero(AtPct)[0], nonzero(GEMSRel)[0]))
+    # Eliminate duplicates and make sure in ascending order.
+    IncludedZ = sort(unique(IncludedZ))
+    # Indices are 0 based, Z is 1 based.
+    IncludedZ += 1
+    # Get the list of element names for those elements.
+    TickLabels = [El for Z, El in enumerate(pb.ElementalSymbols) if Z in IncludedZ]
+    TickInds = list(range(len(TickLabels)))
+    GEMSInds = []
+    GEMSVals = []
+    GEMSErrs = []
+    SpectrumInds = []
+    SpectrumVals = []
+    ChondriticInds = []
+    ChondriticVals = []
+    for Zminus1, Val in enumerate(AtPctToSi[:pb.U - 1]):
+        if GEMSRel[Zminus1] > 0:
+            GEMSInds.append(TickLabels.index(pb.ElementalSymbols[Zminus1 + 1]))
+            GEMSVals.append(GEMSRel[Zminus1])
+            GEMSErrs.append(GEMSRelSD[Zminus1])
+        if AtPct[Zminus1] > 0:
+            SpectrumInds.append(TickLabels.index(pb.ElementalSymbols[Zminus1 + 1]))
+            SpectrumVals.append(AtPctToSi[Zminus1])
+        # This part only applies if not normalizing to chondritic.
+        # if pb.ElementalSymbols[Zminus1+1] in TickLabels:
+        #     ChondriticInds.append(TickLabels.index(pb.ElementalSymbols[Zminus1+1]))
+        #     ChondriticVals.append(ProtosolarToSi[Zminus1])
+    # We will be plotting so clear the plot that may already be plotted.
+
+    
+
+    fig = make_subplots()
+
+    # Scatter plot for GEMS
+    trace1 = go.Scatter(
+        x=GEMSInds,
+        y=GEMSVals,
+        mode='markers',
+        error_y=dict(
+            type='data',
+            array=GEMSErrs,
+            visible=True,
+            thickness=2,
+            color='red'
+        ),
+        marker=dict(
+            size=15,
+            color='red',
+            opacity=0.5
+        ),
+        name=RefStr
+    )
+
+    # Scatter plot for Spectrum
+    trace2 = go.Scatter(
+        x=SpectrumInds,
+        y=SpectrumVals,
+        mode='markers',
+        marker=dict(
+            size=15,
+            symbol='triangle-down',
+            color='blue',
+            opacity=0.5
+        ),
+        name='This Spectrum'
+    )
+
+    # Line at Chondritic
+    trace3 = go.Scatter(
+        x=[0, 92],
+        y=[1, 1],
+        mode='lines',
+        line=dict(
+            color='green',
+            width=3
+        ),
+        name='Chondritic'
+    )
+
+    fig.add_trace(trace1)
+    fig.add_trace(trace2)
+    fig.add_trace(trace3)
+
+    # Set x-axis labels
+    fig.update_xaxes(
+        tickvals=TickInds,
+        ticktext=TickLabels,
+        tickangle=90
+    )
+
+    fig.update_layout(
+        yaxis=dict(
+            type='log',
+            range=[np.log10(3e-2), np.log10(30)]
+        ),
+        yaxis_title='Element/Si/chondritic, At%',
+        autosize=True,
+        legend=dict(
+            orientation='h',
+            yanchor='bottom',
+            y=1.02,
+            xanchor='right',
+            x=1
+        )
+    )
+
+    # fig.show()
+    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    """
+    original code
+    plt.figure(1)
+    plt.clf()
+    # GEMS plot
+    plt.scatter(GEMSInds, GEMSVals, marker='o', color='red', s=150, alpha=0.5, label=RefStr)
+    # plt.errorbar(GEMSInds, GEMSVals, yerr=GEMSErrs, fmt='none', elinewidth=3, capsize=7, capthick=3, ecolor='red')
+    plt.errorbar(GEMSInds, GEMSVals, yerr=GEMSErrs, fmt='none', alpha=0.5, elinewidth=5, capsize=0, capthick=3, ecolor='red')
+    # This spectrum.
+    plt.scatter(SpectrumInds, SpectrumVals, marker='v', color='blue', s=150, alpha=0.5, label='This Spectrum')
+    # Chondritic
+    # plt.scatter(ChondriticInds, ChondriticVals, marker='s', color='green', s=200,alpha=0.5)
+    plt.axhline(1, 0, 92, color='green', linewidth=3, label='Chondritic')
+    plt.xticks(TickInds, TickLabels, rotation='vertical')
+    plt.gca().set_yscale('log')
+    plt.legend()
+    # plt.legend(['Ishii et al., 2008', 'This Spectrum', 'Chondritic'])
+    plt.ylabel('Element/Si/chondritic, At%', fontsize=FontSizeBasis)
+    plt.gca().set_ylim([3e-2, 30])
+    plt.tight_layout()
+    """
+
+
+
+    return GEMSAtPct, GEMSAtPctSD, fig
 
 def PrintTernary(AtPct, GEMSAtPct, GEMSAtPctSD):
 
